a9a233506126b95e2ba0069311857d0cf7d523ca
MrWhatsHisFace <Kingsmo2008@outlook.com>
2021-01-06 13:47:02 +0000

    Grep

--- main.c
+++ main.c
@@ -3,10 +3,37 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+void grep(char *argv[])
+{
+    int fd,r,j=0;
+    char temp,line[100];
+    if((fd=open(argv[2],O_RDONLY)) != -1)
+    {
+        while((r=read(fd,&temp,sizeof(char)))!= 0)
+        {
+            if(temp!='\n')
+            {
+                line[j]=temp;
+                j++;
+            }
+            else
+            {
+                if(strstr(line,argv[1])!=NULL)
+                    printf("%s\n",line);
+                memset(line,0,sizeof(line));
+                j=0;
+            }
+
+        }
+    }   
+}
 
 int main()
 {
-    struct stat st = {0};
     puts("Making directory and file...");
     FILE* test;
     sleep(1);
